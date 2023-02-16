08ea80c6aff3a9c78722910ee39f5e6f7da7f0ac
Liam <Liam@DESKTOP-N30JS3I>
2023-02-16 09:34:48 -0600

    Add project files.

---
+++ LaptopVendorMVC/Data/LaptopVendorContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace LaptopVendorMVC.Data
+{
+    public class LaptopVendorContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+    }
+}
